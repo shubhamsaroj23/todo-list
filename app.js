a472b5afb5fcb323dc3c8692a64de5643f2aa32d
shubhamsaroj23 <67384961+shubhamsaroj23@users.noreply.github.com>
2021-07-16 02:13:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+
+function getAndUpdate(){
+    console.log("Updating List...");
+    tit = document.getElementById('title').value;
+    desc = document.getElementById('description').value;
+    if (localStorage.getItem('itemsJson')==null){
+        itemJsonArray = [];
+        itemJsonArray.push([tit, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    }
+    else{
+        itemJsonArrayStr = localStorage.getItem('itemsJson')
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+        itemJsonArray.push([tit, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    }
+    update();
+}
+
+function update(){
+    if (localStorage.getItem('itemsJson')==null){
+        itemJsonArray = []; 
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    } 
+    else{
+        itemJsonArrayStr = localStorage.getItem('itemsJson')
+        itemJsonArray = JSON.parse(itemJsonArrayStr); 
+    }
+    // Populate the table
+    let tableBody = document.getElementById("tableBody");
+    let str = "";
+    itemJsonArray.forEach((element, index) => {
+        str += `
+        <tr>
+        <th scope="row">${index + 1}</th>
+        <td>${element[0]}</td>
+        <td>${element[1]}</td> 
+        <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td> 
+        </tr>`; 
+    });
+    tableBody.innerHTML = str;
+}
+add = document.getElementById("add");
+add.addEventListener("click", getAndUpdate);
+update();
+function deleted(itemIndex){
+    console.log("Delete", itemIndex);
+    itemJsonArrayStr = localStorage.getItem('itemsJson')
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+    // Delete itemIndex element from the array
+    itemJsonArray.splice(itemIndex, 1);
+    localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+    update();
+
+}
+function clearStorage(){
+    if (confirm("Do you areally want to clear?")){
+    console.log('Clearing the storage')
+    localStorage.clear();
+    update()
+    }
+}
+
+// for changing theam 
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const themeStylesheet = document.getElementById('theme');
+    const storedTheme = localStorage.getItem('theme');
+    if(storedTheme){
+        themeStylesheet.href = storedTheme;
+    }
+    const themeToggle = document.getElementById('theme-toggle');
+    themeToggle.addEventListener('click', () => {
+        // if it's light -> go dark
+        if(themeStylesheet.href.includes('light')){
+            themeStylesheet.href = 'dark-theme.css';
+            themeToggle.innerText = 'Switch to light mode';
+        } else {
+            // if it's dark -> go light
+            themeStylesheet.href = 'light-theme.css';
+            themeToggle.innerText = 'Switch to dark mode';
+        }
+        // save the preference to localStorage
+        localStorage.setItem('theme',themeStylesheet.href)  
+    })
+})
